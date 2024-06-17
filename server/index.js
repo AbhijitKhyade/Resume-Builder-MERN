b172b07f9534d24bd49574de0fb6cda475809375
AbhijitKhyade <abhijitkhyade@gmail.com>
2024-06-17 22:43:35 +0530

    Changes in server index page

--- server/index.js
+++ server/index.js
@@ -16,21 +16,32 @@ mongoDB();
 
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 //middlewares
-// app.use(cors({
-//     origin: 'https://resume-builder-mern-eight.vercel.app',
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: 'https://resume-builder-mern-eight.vercel.app',
+    credentials: true,
+}));
 
 //routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/data', resumeRoutes);
 
-const PORT = process.env.PORT || 8080;
+//middleware for logging
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 
 app.listen(process.env.PORT, () => {
-     console.log(`Server is working on https://resume-builder-mern-eight.vercel.app:${process.env.PORT}`);
+    console.log(`Server is working on https://resume-builder-mern-eight.vercel.app:${process.env.PORT}`);
     //console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
